375e106d6dbaf88524a9a7427b877781bb7b7494
Martin Muñoz <im.mmun@gmail.com>
2017-05-10 10:50:02 -0400

    wip

---
+++ src/hello.test.ts
@@ -0,0 +1,11 @@
+import hello from './hello';
+import { expect } from 'chai';
+
+describe('Hello function', () => {
+  for (let i = 1; i <= 10; i++) {
+    it(`should return hello world: ${i}`, () => {
+      const result = hello();
+    expect(result).to.equal(i < 16 ? 'Hello World!' : 'lol!  ');
+    });
+  }
+});
